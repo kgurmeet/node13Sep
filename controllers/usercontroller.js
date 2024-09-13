92c083e22d78fad4f978a203c1daa277792391b7
kgurmeet <gkaur557391@gmail.com>
2024-09-13 12:50:32 +0530

    updated file

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -7,6 +7,18 @@ function getAllUsers(req,res){
 
     }
 }
+function getUser(req,res){
+    try{
+        let id=parseInt(req.params.id)
+        let user=users.find((user)=>user.id===id);
+        res.json(user);
+
+    }catch(err){
+
+    }
+}
+
 module.exports={
-    getAllUsers
+    getAllUsers,
+    getUser
 }
